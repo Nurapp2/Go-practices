b3cefbb474dca6402c9ee5fc3e16f59513f491d7
Nurapp2 <Nurbakyt_017@outlook.com>
2025-12-18 01:11:32 +0500

    Refactor Vote struct and update Repository interface

--- polling-system/internal/domain/vote/model.go
+++ polling-system/internal/domain/vote/model.go
@@ -6,17 +6,25 @@ import (
 )
 
 type Vote struct {
-	ID        int64     `json:"id"`
-	PollID    int64     `json:"poll_id"`
-	OptionID  int64     `json:"option_id"`
-	UserID    int64     `json:"user_id"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int64
+	PollID    int64
+	OptionID  int64
+	UserID    int64
+	CreatedAt time.Time
 }
 
 type Repository interface {
 	Create(ctx context.Context, v *Vote) error
-	CountByPoll(ctx context.Context, pollID int64) (map[int64]int64, int64, error)
-	AggregatedByPoll(ctx context.Context, pollID int64) (map[int64]int64, int64, error)
-	IncrementAggregated(ctx context.Context, pollID, optionID int64) error
 	GetPollStatus(ctx context.Context, pollID int64) (string, error)
+
+	GetPollWindow(ctx context.Context, pollID int64) (*time.Time, *time.Time, error)
+
+	AggregatedByPoll(ctx context.Context, pollID int64) (map[int64]int64, int64, error)
+	CountByPoll(ctx context.Context, pollID int64) (map[int64]int64, int64, error)
+	GetVotesByUser(ctx context.Context, userID int64) ([]UserVote, error)
+}
+type UserVote struct {
+	PollID   int64     `json:"poll_id"`
+	OptionID int64     `json:"option_id"`
+	VotedAt  time.Time `json:"voted_at"`
 }
